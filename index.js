5aea66e23ea011e4e64590ed88ef322b4f4f680f
bishwajitP27 <bpradhan@apptio.com>
2022-02-22 21:53:15 +0530

    Iframes replaced with code block to remove unnecessary http calls

--- index.js
+++ index.js
@@ -8,4 +8,45 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             top: target, behavior: 'smooth'
         });
     });
-  });
+});
+
+//Convert HTML code to text
+function convertHTMLToCodeSnippet(code) {
+    return code.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+}
+
+function generateSnippetCode(sectionId) {
+    const componentElemet = document.getElementById(`${sectionId}-code`) 
+    const snippetElement = document.getElementById(`${sectionId}-snippet`);
+    snippetElement.innerHTML = convertHTMLToCodeSnippet(componentElemet.innerHTML);
+}
+
+// Generate Alert Snippet
+generateSnippetCode("alert");
+
+// Generate Avatar Snippet
+generateSnippetCode("avatar");
+
+// Generate Badge Snippet
+generateSnippetCode("badge");
+
+// Generate Button Snippet
+generateSnippetCode("button");
+
+// Generate Card Snippet
+generateSnippetCode("card");
+
+// Generate Image Snippet
+generateSnippetCode("image");
+
+// Generate Input Snippet
+generateSnippetCode("input");
+
+// Generate Text Utility Snippet
+generateSnippetCode("text-utility");
+
+// Generate List Snippet
+generateSnippetCode("list");
+
+// Generate Navigation Snippet
+generateSnippetCode("navigation");
